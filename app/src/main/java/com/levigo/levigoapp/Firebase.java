ebbb20bdf427cb71eda7ccdb5cd3e5edf902b143
Davit Barblishvili <db3230@columbia.edu>
2020-06-24 23:38:38 -0400

    added 3 components to manual_entry layout --> Top layer with two buttons, barcode textEdit, and equipment name textEdit

--- app/src/main/java/com/levigo/levigoapp/Firebase.java
+++ app/src/main/java/com/levigo/levigoapp/Firebase.java
@@ -47,5 +47,6 @@ public class Firebase extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
     }
 }
