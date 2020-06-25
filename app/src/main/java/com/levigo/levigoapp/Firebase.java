4b35375b802abf8eb354b546e058249c6164afc3
Davit Barblishvili <db3230@columbia.edu>
2020-06-25 12:34:24 -0400

    Added Firebase class where input from the user is taken and submitted to the database. Added InventoryTemplate class that is used to store data in database. Added Manual Entry Simple form to work on Database.

--- app/src/main/java/com/levigo/levigoapp/Firebase.java
+++ app/src/main/java/com/levigo/levigoapp/Firebase.java
@@ -1,32 +1,26 @@
 package com.levigo.levigoapp;
 import java.util.Calendar;
 import android.app.Activity;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Firebase extends Activity {
     // Firebase database
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference equipRef;
 
-    private static final String TAG = "MainActivity";
-    // TAGS FOR THE FIELD NAMES
-    private String barcode_field = "BARCODE";
-    private String name_field = "NAME";
-    private String equipment_type_field = "EQUIPMENT_TYPE";
-    private String manufacturer_field = "MANUFACTURER";
-    private String procedure_used_field = "PROCEDURE";
-    private String procedure_date_field = "PROCEDURE_DATE";
-    private String patiend_id_field = "PATIENT_ID";
-    private String expiration_field = "EXPIRATION";
-    private String quantity_field = "QUANTITY";
-    private String hospital_name_field = "HOSPITAL_NAME";
-    private String physical_location_field = "PHYSICAL_LOCATION";
-    private String notes_field = "NOTES";
-    private String current_date_time_field = "CURRENT_TIME";
+    private static final String TAG = "Firebasectivity";
 
     // USER INPUT VALUES
     private EditText barcode;
@@ -38,15 +32,72 @@ public class Firebase extends Activity {
     private EditText patient_id;
     private EditText expiration;
     private EditText quantity;
-    private String current_date_time;
     private EditText hospital_name;
     private EditText physical_location;
     private EditText notes;
 
+    // method for saving data to firebase cloud firestore
+    public void save_data(View view){
+
+        //assigning user input values to string parameters
+        // naming each document with barcode number of the equipment
+        /* each document needs (which will include an equipment) a document number. I am assigning
+        barcode number of the equipment to a document number. it is possible to have random id as
+        a document number as well
+         */
+        String barcode_str = barcode.getText().toString();
+        equipRef = db.document("Inventory/" + barcode_str);
+
+        String name_str = name.getText().toString();
+        String equipment_type_str = equipment_type.getText().toString();
+        String manufacturer_str = manufacturer.getText().toString();
+        String procedure_used_str = procedure_used.getText().toString();
+        String procedure_date_str = procedure_date.getText().toString();
+        String patient_id_str = patient_id.getText().toString();
+        String expiration_str = expiration.getText().toString();
+        String quantity_str = "2"; // temporarily
+        String current_date_time = Calendar.getInstance().getTime().toString();
+        String hospital_name_str = hospital_name.getText().toString();
+        String physical_location_str = physical_location.getText().toString();
+        String notes_str  = notes.getText().toString();
+        InventoryTemplate in = new InventoryTemplate(barcode_str,name_str, equipment_type_str,
+                manufacturer_str,procedure_used_str, procedure_date_str,patient_id_str,expiration_str,
+                quantity_str, hospital_name_str, current_date_time, physical_location_str, notes_str);
+
+        //saving data of InventoryTemplate to database
+        equipRef.set(in)
+                //in case of success
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Firebase.this, "equipment saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                // in case of failure
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Firebase.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entry);
 
+        setContentView(R.layout.activity_entry);
+        barcode = findViewById(R.id.barcode_editText);
+        name = findViewById(R.id.name_editText);
+        equipment_type = findViewById(R.id.equipment_editText);
+        manufacturer = findViewById(R.id.manufacturer_editText);
+        procedure_used =  findViewById(R.id.procedure_editText);
+        procedure_date = findViewById(R.id.procedureDate_editText);
+        patient_id = findViewById(R.id.patientID_editText);
+        expiration = findViewById(R.id.expiration_editText);
+        hospital_name = findViewById(R.id.hospitalName_editText);
+        physical_location = findViewById(R.id.physicalLocation_editText);
+        notes = findViewById(R.id.notes_editText);
     }
 }
