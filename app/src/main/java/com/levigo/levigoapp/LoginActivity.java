b1fa8811bcec53d8e9c4177a1177e677c2dbc79d
tupleu <briantehwu@gmail.com>
2020-06-26 21:33:07 -0400

    Created LoginActivity

---
+++ app/src/main/java/com/levigo/levigoapp/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.levigo.levigoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private boolean clear;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+
+        clear = true;
+        userIsLoggedIn();
+
+        setContentView(R.layout.activity_login);
+
+    }
+    private void userIsLoggedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            clear = false;
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+            return;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        if(clear) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
+        }
+        super.onStop();
+    }
+}
